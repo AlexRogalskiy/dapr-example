ae047ce26e021c061b2015a49f4755b6e7255266
Kevin_T <596823919@qq.com>
2021-03-24 20:10:13 +0800

    feat: add http/grpc dapr example

---
+++ src/main/java/com/reactive/dapr/example/grpc/HelloWorldClient.java
@@ -0,0 +1,44 @@
+package com.reactive.dapr.example.grpc;
+
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+import io.dapr.client.domain.HttpExtension;
+import reactor.core.publisher.Mono;
+
+/**
+ * 1. Build and install jars:
+ * mvn clean install
+ * 2. cd [repo root]/examples
+ * 2. Send messages to the server:
+ * dapr run -- java -jar target/dapr-java-sdk-examples-exec.jar io.dapr.examples.invoke.grpc.HelloWorldClient
+ */
+public class HelloWorldClient {
+
+    /**
+     * The main method of the client app.
+     *
+     * @param args Array of messages to be sent.
+     */
+    public static void main(String[] args) throws Exception {
+        try (DaprClient client = new DaprClientBuilder().build()) {
+
+            String serviceAppId = "hellogrpc";
+            String method = "say";
+
+            int count = 0;
+            while (true) {
+                String message = "Message #" + (count++);
+                System.out.println("Sending message: " + message);
+                Mono<Void> invokeMethod = client.invokeMethod(serviceAppId, method, message, HttpExtension.NONE);
+                invokeMethod.block();
+                System.out.println("Message sent: " + message);
+
+                Thread.sleep(1000);
+
+                // This is an example, so for simplicity we are just exiting here.
+                // Normally a dapr app would be a web service and not exit main.
+                System.out.println("Done");
+            }
+        }
+    }
+}
